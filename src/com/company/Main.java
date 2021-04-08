aea4f3178bb8ca039cbd7f9b216a234197a31793
paul obiero <paulobiero147@gmail.com>
2021-04-08 23:03:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        PiJavaUltrasonic sonic=new PiJavaUltrasonic(
+                0,//ECO 11
+                1,//TRIG 22
+                1000,//REJECTION_START ; long
+                23529411 //REJECTION_TIME ; long
+        );
+        System.out.println("Start");
+        while(true){
+            System.out.println("distance "+sonic.getDistance()+"mm");
+            Thread.sleep(1000); //1s
+        }
+    }
+}
